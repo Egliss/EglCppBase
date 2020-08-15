9e0eb9a7f36156b82d61b5b5fb4404a771dfdd8f
Egliss <syunya.tokunaga@drecom.co.jp>
2020-08-15 16:09:19 +0900

    Add SpriteFont default font batch

---
+++ EglCpp/Command/SpriteFontGenerator.bat
@@ -0,0 +1,2 @@
+chcp 932
+..\External\DirectXTK\MakeSpriteFont\bin\Debug\MakeSpriteFont.exe "ÇlÇr ÉSÉVÉbÉN" ..\Resources\Fonts\default.spritefont /FontSize:12 /CharacterRegion:32-126 /CharacterRegion:0x3000-0x30ff  /CharacterRegion:0xff01-0xffe5 /CharacterRegion:0x4e00-0x9fff /FastPack
